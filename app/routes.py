22ec4dd5a1eb1001497924fb9d6d25d7983ba5a4
Erika Sha <erikaasha05@gmail.com>
2022-10-24 13:46:24 -0500

    created endpoints for getting one book and addressing errors - 400 and 404

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from crypt import methods
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Planet:
     def __init__(self, id, name, description, color):
@@ -7,6 +7,14 @@ class Planet:
         self.name = name
         self.description = description
         self.color = color
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "color": self.color
+        }
 
 planets = [Planet(1, 'Mercury', 'closest to sun', 'gray'),
 Planet(2, 'Mars', 'closest to Earth', 'red'),
@@ -19,12 +27,24 @@ planet_bp = Blueprint("planets", __name__, url_prefix="/planets")
 def get_planets():
     planet = []
     for item in planets:
-        planet.append({
-            "id": item.id,
-            "name": item.name,
-            "description": item.description,
-            "color": item.color
-        })
+        planet.append(item.to_json())
 
     return jsonify(planet)
+
+def validate_id(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message": f"planet {planet_id} is invalid"}, 400))
+
+    for planet in planets:
+        if planet_id == planet.id:
+            return planet
     
+    abort(make_response({"message": f"planet {planet_id} is not found"}, 404))
+
+@planet_bp.route("/<id>", methods=["GET"])
+def handle_planet(id):
+    planet = validate_id(id)
+
+    return jsonify(planet.to_json())
